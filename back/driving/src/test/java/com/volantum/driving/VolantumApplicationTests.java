59609d38de60238413bf6b2d7d056b50b6f3185c
Rodrigo Martinez <rodrigo@1hv.es>
2025-04-18 19:48:40 +0200

    Backend Initial Commit

---
+++ back/driving/src/test/java/com/volantum/driving/VolantumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volantum.driving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolantumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
