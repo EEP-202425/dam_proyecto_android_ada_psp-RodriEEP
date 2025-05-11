9e02160775601a14d280b92f8dddfbea39c70ca1
Rodrigo Martinez <rodrigo@1hv.es>
2025-05-12 00:27:12 +0200

    Theme config

---
+++ front/app/src/main/java/com/example/volantum/ui/theme/SouceSans.kt
@@ -0,0 +1,12 @@
+package com.example.volantum.ui.theme
+
+import com.example.volantum.R
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val SourceSans = FontFamily(
+    Font(R.font.source_sans_bold, weight= FontWeight.Bold),
+    Font(R.font.source_sans_regular, weight = FontWeight.Normal),
+    Font(R.font.source_sans_semi_bold, weight = FontWeight.SemiBold)
+)
